53eaa86d82df6a10e82bd4286f57dd2acca77202
Felipe Penteado <flpenteado@gmail.com>
2026-07-05 14:38:21 -0300

    small update

--- src/main/java/com/example/task_manager/dto/TaskRequest.java
+++ src/main/java/com/example/task_manager/dto/TaskRequest.java
@@ -3,7 +3,7 @@ package com.example.task_manager.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+// import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
